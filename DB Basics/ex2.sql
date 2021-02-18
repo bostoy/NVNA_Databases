6fed59869d848e8b0136165aa98ad4a176f611e5
Borislav Stoyanov <bostoy@protonmail.com>
2021-02-18 10:59:00 +0200

    exercise 2

---
+++ DB Basics/ex2.sql
@@ -0,0 +1,14 @@
+DESCRIBE departments;
+SELECT * FROM departments;
+
+DESCRIBE employees;
+SELECT employee_id, last_name,job_id, hire_date FROM employees;
+SELECT employee_id FROM employees;
+SELECT DISTINCT job_id FROM employees;
+SELECT employee_id "Emp #", last_name "Employee", job_id "Job", hire_date "Hire Date" FROM employees;
+
+-- || string concatenation
+SELECT last_name||','||job_id "Employee and Title" FROM employees; 
+
+SELECT employee_id||','||first_name||','||last_name||','||email||','||phone_number||
+','||job_id||','||manager_id||','||hire_date||','||salary||','||commission_pct||','||department_id THE_OUTPUT FROM employees;
