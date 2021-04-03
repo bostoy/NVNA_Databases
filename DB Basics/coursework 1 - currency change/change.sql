ef31989e8c38f048f4f469d31816b7a30cf0fda1
Borislav Stoyanov <bostoy@protonmail.com>
2021-04-03 14:58:25 +0300

    DB-Coursework Progress

--- DB Basics/coursework 1 - currency change/change.sql
+++ DB Basics/coursework 1 - currency change/change.sql
@@ -1,24 +1,57 @@
-CREATE TABLE Currencies (
-currency_id INTEGER UNIQUE ,
-description VARCHAR2(50),
-buy_price FLOAT(2),
-sell_price FLOAT(2)
+CREATE TABLE currencies 
+(
+currency_id INTEGER UNIQUE,
+currency_name VARCHAR2(20) UNIQUE,
+currency_description VARCHAR(50),
+sell_price FLOAT(5),
+buy_price FLOAT(5),
+CONSTRAINT currency_pk PRIMARY KEY(currency_id)
 );
 
-DESCRIBE Currencies;
-
-CREATE TABLE Clients (
+CREATE TABLE clients
+(
 client_id INTEGER UNIQUE,
-uniform_civil_number VARCHAR2(10) UNIQUE,
-first_name VARCHAR2(20),
-last_name VARCHAR2(20)
-);
-DESCRIBE Clients;
-
-CREATE TABLE Orders (
-client_id INTEGER  FOREIGN KEY REFERENCES Clients(client_id), --error missing right parenthesis
-order_number INTEGER UNIQUE,
-order_date DATE
+client_first_name VARCHAR(20),
+client_last_name VARCHAR(20),
+client_egn  VARCHAR(10),
+CONSTRAINT client_pk PRIMARY KEY(client_id)
 );
 
-DESCRIBE Orders;
+CREATE TABLE orders
+(
+client_id INTEGER,
+order_number INTEGER,
+order_date DATE,
+CONSTRAINT client_fk  
+  FOREIGN KEY (client_id)
+  REFERENCES clients(client_id)
+);
+
+CREATE TABLE order_details
+(
+client_id INTEGER,
+currency_name VARCHAR2(20),
+buy_price FLOAT(5),
+sell_price FLOAT(5),
+--kakvo tochno tr da sumira sumata ot zadanieto??
+nqkvasuma INTEGER,
+CONSTRAINT client_fk  
+  FOREIGN KEY (client_id)
+  REFERENCES clients(client_id),
+  
+  CONSTRAINT currency_name_pk  
+  FOREIGN KEY (currency_name)
+  REFERENCES currencies(currency_name),
+  
+  --kak da vzema buy sell sprqmo imeto na valutata i da gi dobavq vuv foreign key??
+  
+  
+  
+--CONSTRAINT buy_price_fk  
+--  FOREIGN KEY (buy_price)
+--  REFERENCES currencies(buy_price),
+--  
+--CONSTRAINT sell_price_fk  
+--  FOREIGN KEY (sell_price)
+--  REFERENCES currencies(sell_price)
+)
